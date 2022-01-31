b67306770863c03c0e2bc26db3e035f6445f8a45
DMelnev <59581747+DMelnev@users.noreply.github.com>
2022-01-31 20:18:16 +0300

    hard leetcode

--- InputOutput/src/main/java/Main.java
+++ InputOutput/src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Class Main
@@ -13,18 +15,26 @@ public class Main {
 //        Solution solution = new Solution();
 //        System.out.println(solution.romanToInt("IIIIIIIIII"));
 
-        File directory = new File("fold/fold/fold");
-        File file = new File("fold/fold/fold/file1.txt");
+        File directory = new File("fold1/fold2/fold3");
+        File file1 = new File("fold1/fold2/fold3/asd.txt");
+        File file2 = new File("fold1/fold2/fold3/qwe.txt");
+        File file3 = new File("fold1/fold2/fold3/zxc.txt");
         try {
             directory.mkdirs();
-            file.createNewFile();
+            file1.createNewFile();
+            file2.createNewFile();
+            file3.createNewFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(file.getName());
-        System.out.println(file.exists());
-        System.out.println(file.isFile());
-        System.out.println(directory.isDirectory());
+//        System.out.println(file.getName());
+//        System.out.println(file.exists());
+//        System.out.println(file.isFile());
+//        System.out.println(directory.isDirectory());
+        for(File file : Objects.requireNonNull(directory.listFiles((dir, name) -> name.contains("w")))){
+//            for(File file : directory.listFiles((dir, name) -> name.contains("w"))){
+            System.out.println(file.getAbsolutePath());
+        }
     }
 }
 
